7cd619d24e67936b6d716a911e3a1e6da2690070
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2022-09-07 22:31:24 +0300

    task 1 (step1-2)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import PropTypes from 'prop-types';
+import { Component } from "react";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+  
+  leaveFeedback = (e) => {
+    const { name } = e.target;
+    console.log(name);
+    this.setState(prevState => (
+      { [name]: prevState[name] + 1 }));
+  }
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const {good} = this.state;
+    const total = this.countTotalFeedback();
+    const goodPercentage = (good==0 ? 0 : Math.round((good / total) * 100)) + '%';
+    return goodPercentage;
+
+  }
+    
+  render() {
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+        <div>
+          <button type='button' name='good' onClick={this.leaveFeedback}>Good</button>
+          <button type='button' name='neutral' onClick={this.leaveFeedback}>Neutral</button>
+          <button type='button' name='bad' onClick={this.leaveFeedback}>Bad</button>
+        </div>
+        <div>
+          <h2>Statistics</h2>
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+          <p>Total: {this.countTotalFeedback()} </p>
+          <p>Positive feedback: {this.countPositiveFeedbackPercentage()} </p>
+        </div>
+      </>
+    );
+  };
+}
