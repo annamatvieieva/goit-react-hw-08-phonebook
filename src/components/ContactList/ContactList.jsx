f3a06c036079b6d1c13ce9732ddd71e214e8e58a
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2022-12-06 10:56:06 +0200

    finish

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -6,15 +6,11 @@ import { deleteContact } from 'redux/slices/contactsSlice';
 export const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(getContacts);
-  console.log('contacts', contacts);
   const filter = useSelector(getFilter);
-  console.log(filter);
   const normalizeFilter = filter.toLocaleLowerCase();
   const filterContacts = contacts.filter(({ name }) =>
     name.toLowerCase().includes(normalizeFilter)
   );
-  console.log('filterContacts', filterContacts);
-  console.log('normalizeFilter', normalizeFilter);
 
   return (
     <ContactListBox>
