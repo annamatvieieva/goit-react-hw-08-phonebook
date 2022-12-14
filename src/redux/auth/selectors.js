5ae4dcd22bc3634e0b9bc1293127287af7f88c3c
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2022-12-14 20:22:25 +0200

    fixed error

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,5 +1,3 @@
-import { createSelector } from '@reduxjs/toolkit';
-
 export const selectUser = state => state.auth.user;
 export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectIsRefreshing = state => state.auth.isRefreshing;
